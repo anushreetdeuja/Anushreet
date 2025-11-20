3e74f8deea45747a91627a77c1ebd80774bb70dd
Anushreet <anushreetdeuja@gmail.com>
2025-11-20 10:19:42 +0545

    Initial sharing of project

---
+++ bap.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class bap here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class bap
+{
+    int age; // instance variable
+    static int qty; // static variable
+    public static void main(String[] args){
+        
+        int n1=10; // local variable
+        
+        //Implicit Typecasting
+        
+        double dt= n1;
+        System.out.println(dt);
+    }
+}
