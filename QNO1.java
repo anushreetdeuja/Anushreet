18d7d7b99f21430aa938ec66975caed11483dbad
Anushreet <anushreetdeuja@gmail.com>
2025-11-20 11:05:03 +0545

    QNO4 done

---
+++ QNO1.java
@@ -0,0 +1,32 @@
+/**
+ * Write a description of class DefaultValues here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class QNO1 {
+    // Member variables (fields) for all 8 primitive types
+    byte b;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+    char c;
+    boolean bool;
+
+    public static void main(String[] args) {
+        // Create an instance of the class
+        QNO1 obj = new QNO1();
+
+        // Print the default values of each field
+        System.out.println("byte default: " + obj.b);
+        System.out.println("short default: " + obj.s);
+        System.out.println("int default: " + obj.i);
+        System.out.println("long default: " + obj.l);
+        System.out.println("float default: " + obj.f);
+        System.out.println("double default: " + obj.d);
+        System.out.println("char default: '" + obj.c + "'");
+        System.out.println("boolean default: " + obj.bool);
+    }
+}
--- week3.java
+++ week3.java
@@ -6,7 +6,15 @@
  */
 public class week3
 {
+    int age;//instance variable
+    static int qty;//static variable
     public static void main(String[] args) {
-        System.out.println("Hello, this is week3!");
+       int agr=10;//local variable
+       System.out.println(agr);
+       // <classname> <variable>=new <classname>();
+       week3 w1=new week3();
+       
+       System.out.println(w1.age);
+       System.out.println(qty);
     }
 }
